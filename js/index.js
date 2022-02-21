17609a8b6ad92751bb0f0996f04d4074de06c3bd
Marco Zampini <marco@bloom-it.io>
2022-02-21 18:18:11 +0100

    Bonus 1

--- js/index.js
+++ js/index.js
@@ -20,23 +20,33 @@ if (hacker1.length > hacker2.length) {
 }
 // Iteration 3: Loops
 const hacker1Uppercase = hacker1.toUpperCase();
-let hacker1CapitalLetters = '';
+let hacker1CapitalLetters = "";
 for (let i = 0; i < hacker1Uppercase.length; i++) {
   hacker1CapitalLetters = `${hacker1CapitalLetters} ${hacker1Uppercase[i]}`;
 }
 
 console.log(hacker1CapitalLetters.slice(1, hacker1CapitalLetters.length));
 
-let hacker2Reverse = '';
-for(let i=hacker2.length - 1; i >= 0; i--) {
-   hacker2Reverse = `${hacker2Reverse}${hacker2[i]}`
+let hacker2Reverse = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  hacker2Reverse = `${hacker2Reverse}${hacker2[i]}`;
 }
-console.log(hacker2Reverse)
+console.log(hacker2Reverse);
 
-if(hacker1 < hacker2) {
-    console.log(`The driver's name goes first.`);
-} else if(hacker1 > hacker2) {
-    console.log(`Yo, the navigator goes first definitely.`);
+if (hacker1 < hacker2) {
+  console.log(`The driver's name goes first.`);
+} else if (hacker1 > hacker2) {
+  console.log(`Yo, the navigator goes first definitely.`);
 } else {
-    console.log(`What?! You both have the same name?`);
+  console.log(`What?! You both have the same name?`);
 }
+
+let paragraphs = `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.`;
+
+const paragraphsSubstring = paragraphs.split(" ");
+console.log(paragraphsSubstring.length);
+
+const paragraphsNumberOfEt = paragraphs.split(" et ");
+console.log(paragraphsNumberOfEt.length - 1);
